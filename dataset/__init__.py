03e4cef0b980564c92001a5a9ccdd61a26a2d44e
pengliuhan <103926520+pengliuhan@users.noreply.github.com>
2022-12-21 09:53:25 +0800

    Create __init__.py

---
+++ dataset/__init__.py
@@ -0,0 +1,7 @@
+from .vimeo90k import Vimeo90KDataset, VideoTestVimeo90KDataset
+from .ovqe_dataset import OVQEDataset
+
+__all__ = [
+    'Vimeo90KDataset', 'VideoTestVimeo90KDataset', 
+    'OVQEDataset',
+    ]
